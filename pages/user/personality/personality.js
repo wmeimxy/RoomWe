fe9f05ea06668aa3a89be002e81e6accce25336c
大骨熬成了汤 <oncwnuFmZMhEL6PnQHOW3CvLTznE@git.weixin.qq.com>
2021-11-14 23:42:34 -0500

    complete personality test

--- pages/user/personality/personality.js
+++ pages/user/personality/personality.js
@@ -1,4 +1,19 @@
+const years = ["ESTJ","ESTP","ESFJ","ESFP","ENTJ", "ENTP","ENFJ","ENFP","ISTJ","ISTP","ISFJ","ISFP","INTJ", "INTP","INFJ","INFP"]
+
 Page({
+  onLoad: function (options) {
+  var mbti = wx.getStorageSync('MBTI')
+  },
+
+  data: {
+    years: years,
+    },
+    bindChange: function(e) {
+    this.setData({
+    year: this.data.years[0],
+    })
+    },
+     
 	yes() {
         wx.navigateTo({
           url: './yes/yes',
@@ -6,6 +21,10 @@ Page({
     },
 
     gotombti(options) {
-
+        wx.navigateTo({
+          url: './no/no',
+        })
     }
+    
 })
+
